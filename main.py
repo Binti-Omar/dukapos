367dcf5da09431629a8fc943e70d4ba213698060
Binti <chombobintiomar5@gmail.com>
2026-08-05 21:04:01 +0300

    session

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from flask import Flask,render_template,request,redirect,url_for
+from flask import Flask,render_template,request,redirect,url_for,session,flash
 from database import display_products,display_sales,display_stock,insert_products,insert_sales,insert_stock,insert_user,check_email
+from flask_bcrypt import Bcrypt
+from functools import wraps
+
 
 app = Flask(__name__)
+bcrypt = Bcrypt(app)
+app.secret_key="kdjuiwehdjslmcmdjvbef"
 
 @app.route("/")
 def home():
     return render_template("index.html")
-    
+
+def login_required(f):
+    @wraps(f) 
+    def protected(*args,**kwargs):
+        if 'email' not in session:
+            return redirect(url_for('login'))
+        return f(*args,**kwargs)
+    return protected
 
 @app.route("/products")
+@login_required
 def products():
     products=display_products()
     return render_template("products.html",products=products)
@@ -22,12 +35,11 @@ def add_products():
         new_product=(product_name,buying_price,selling_price)
         # print(new_product)
         insert_products(new_product)
+        flash('Products added successfully',"success")
     return redirect(url_for("products"))
 
-
-
-
 @app.route("/sales")
+@login_required
 def sales():
     sales=display_sales()
     products=display_products()
@@ -40,11 +52,11 @@ def make_sale():
         quantity=request.form["quantity"]
         new_sale=(pid,quantity)
         insert_sales(new_sale)
+        flash('Sales added successfully',"success")
     return redirect(url_for("sales"))
 
-
-
 @app.route("/stock")
+@login_required
 def stock():
     stock=display_stock()
     products=display_products()
@@ -57,9 +69,11 @@ def add_stock():
         stock_quantity=request.form["s_quantity"]
         new_stock=(pid,stock_quantity)
         insert_stock(new_stock)
+        flash('Stock added successfully',"succes")
         return redirect(url_for("stock"))
 
 @app.route("/dashboard")
+@login_required
 def dashboard():
     return render_template("dashboard.html")
 
@@ -70,19 +84,42 @@ def register():
         email=request.form["email"]
         password=request.form["password"]
         role=request.form["role"]
-        new_user=(fullname,email,password,role)
+        hashed_password=bcrypt.generate_password_hash(password).decode("utf-8")
+        new_user=(fullname,email,hashed_password,role)
 
         check=check_email(email)
         if check==None:
             insert_user(new_user)
-            print('Registered successfully,please proceed to login',"success")
+            flash('Registered successfully,please proceed to login',"success")
             return redirect(url_for('login'))
         else:
-            print('User already exists use a different email or login please',"warning")
+            flash('User already exists use a different email or login please',"warning")
     return render_template('register.html')
 
-@app.route("/login")
+@app.route("/login",methods=["GET","POST"])
 def login():
+    if request.method=="POST":
+        email=request.form["email"]
+        password=request.form["password"]
+
+        check=check_email(email)
+        if check==None:
+            flash("The email does not exist,please register","danger")
+            return redirect(url_for("register"))
+        else:
+            if bcrypt.check_password_hash(check[3],password):
+                session['email']=email
+                flash('login successfully',"success")
+                return redirect(url_for('dashboard'))
+            else:
+                flash('Incorrect password',"warning")
+                return render_template('login.html')
     return render_template("login.html")
+
+@app.route('/logout')
+def logout():
+    session.pop('email',None)
+    flash('You have been logged out',"danger")
+    return redirect(url_for('login'))
 
 app.run(debug=True)
